36351252d78795594a95e0ba263956b0de19a71a
EmanuelGalli <emanuel.galli@bootcamp-upgrade.com>
2024-07-25 02:17:31 +0200

    Inicio Proyecto Santiago Rios

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Header from './Componentes/Header'
+import Navegacion from './Componentes/Navegacion'
+import Reformas from './Componentes/Reformas'
+
+function App() {
+
+  return (
+    <>
+      <Navegacion />
+      <Header />
+      <Reformas />
+    </>
+  )
+}
+
+export default App
---
+++ src/Componentes/Reformas.jsx
@@ -0,0 +1,128 @@
+export default function Reformas() {
+    return (
+        <>
+      <div>
+        <section className="bg-white dark:bg-gray-900">
+          <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+            <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+              <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+              Transformaciones de Baños de Sueño
+              </h2>
+              <p className="mb-4">
+En el corazón de cada hogar, el baño es un espacio fundamental que debe combinar funcionalidad y estética. Como arquitecto especializado en reformas, me dedico a transformar baños ordinarios en auténticos oasis de confort y estilo. 
+              </p>
+              <p>
+              Desde la planificación inicial hasta la ejecución final, me encargo de cada detalle para asegurarme de que el resultado final no solo cumpla con tus expectativas, sino que las supere. Ya sea que desees un diseño moderno y minimalista o una combinación clásica y elegante, mi objetivo es crear un espacio que refleje tu estilo personal y satisfaga tus necesidades diarias.
+              </p>
+            </div>
+            <div className="grid grid-cols-2 gap-4 mt-8">
+              <img
+                className="w-full rounded-lg"
+                src="https://images.pexels.com/photos/6620861/pexels-photo-6620861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="office content 1"
+              />
+              <img
+                className="mt-4 w-full lg:mt-10 rounded-lg"
+                src="https://images.pexels.com/photos/6620703/pexels-photo-6620703.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="office content 2"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+      <div>
+        <section className="bg-white dark:bg-gray-900">
+          <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+            <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+              <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+              Renovación de Cocinas: Espacios Funcionales y Elegantes
+              </h2>
+              <p className="mb-4">
+              La cocina es el corazón del hogar, un lugar donde se combinan la funcionalidad y el estilo para crear un ambiente acogedor y práctico. Como arquitecto especializado en reformas, me dedico a transformar cocinas comunes en espacios de ensueño que maximicen la eficiencia y el diseño.
+              </p>
+              Desde el diseño inicial hasta la ejecución final, me aseguro de que cada detalle, desde la distribución del espacio hasta la elección de materiales, esté alineado con tus preferencias y necesidades. Ya sea que busques una cocina contemporánea y minimalista o un diseño clásico y elegante, mi misión es ofrecer una renovación que combine estética y funcionalidad de manera excepcional.
+              <p>
+              
+              </p>
+            </div>
+            <div className="grid grid-cols-2 gap-4 mt-8">
+              <img
+                className="w-full rounded-lg"
+                src="https://images.pexels.com/photos/3718434/pexels-photo-3718434.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="office content 1"
+              />
+              <img
+                className="mt-4 w-full lg:mt-10 rounded-lg"
+                src="https://images.pexels.com/photos/6585767/pexels-photo-6585767.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="office content 2"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <div>
+        <section className="bg-white dark:bg-gray-900">
+          <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+            <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+              <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+              Reformas Integrales: Transformación Total de Tu Espacio
+              </h2>
+              <p className="mb-4">
+              Realizar una reforma integral significa transformar por completo un espacio para adaptarlo a tus necesidades y expectativas, desde la redistribución de los ambientes hasta la selección de los acabados finales. Como arquitecto especializado en reformas integrales, me encargo de cada aspecto del proceso, garantizando una coordinación perfecta entre diseño, planificación y ejecución. 
+              </p>
+              Mi enfoque es ofrecer soluciones personalizadas que optimicen la funcionalidad y estética de tu hogar, abordando cada detalle con precisión y creatividad. Ya sea que se trate de una renovación completa de un apartamento o de una remodelación extensiva de una casa, mi objetivo es crear un ambiente renovado y armonioso que mejore tu calidad de vida y refleje tu estilo personal.
+              <p>
+              
+              </p>
+            </div>
+            <div className="grid grid-cols-2 gap-4 mt-8">
+              <img
+                className="w-full rounded-lg"
+                src="https://images.pexels.com/photos/5644353/pexels-photo-5644353.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="office content 1"
+              />
+              <img
+                className="mt-4 w-full lg:mt-10 rounded-lg"
+                src="https://images.pexels.com/photos/24304750/pexels-photo-24304750/free-photo-of-sofa-en-sala-de-estar-moderna-con-ventanas-de-piso-a-techo.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="office content 2"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <div>
+        <section className="bg-white dark:bg-gray-900">
+          <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+            <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+              <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+              Transformaciones a Medida para Hogares y Comercios
+              </h2>
+              <p className="mb-4">
+              Las reformas integrales ofrecen la oportunidad de transformar completamente un espacio, ya sea un hogar, un local comercial o una oficina, adaptándolo a nuevas necesidades y objetivos. Como arquitecto especializado en reformas integrales, me encargo de todos los aspectos del proceso, desde la planificación inicial hasta la ejecución final.
+              </p>
+             
+              <p>
+              Mi enfoque es crear soluciones personalizadas que maximicen tanto la funcionalidad como la estética del espacio. Para hogares, aseguro que cada área se adapte a tu estilo y necesidades diarias. En el ámbito comercial, optimizo el diseño para mejorar la experiencia del cliente y la operatividad del negocio. En oficinas, mi objetivo es crear entornos de trabajo eficientes y estimulantes que fomenten la productividad. Cada proyecto se maneja con precisión y creatividad para ofrecer resultados que reflejen tu visión y objetivos.
+              </p>
+            </div>
+            <div className="grid grid-cols-2 gap-4 mt-8">
+              <img
+                className="w-full rounded-lg"
+                src="https://images.pexels.com/photos/380768/pexels-photo-380768.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="office content 1"
+              />
+              <img
+                className="mt-4 w-full lg:mt-10 rounded-lg"
+                src="https://images.pexels.com/photos/2253643/pexels-photo-2253643.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="office content 2"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+
+      </>
+    );
+  }
