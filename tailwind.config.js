966a267163afa9a3e7024d8f782e77b6370da3cc
EmanuelGalli <emanuel.galli@bootcamp-upgrade.com>
2024-08-22 11:34:08 +0200

    Modificaciones para que funcione en modo movil la pestaña de servicios

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,15 @@
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Incluye todos los archivos .js, .jsx, .ts, .tsx en el directorio src
+    './public/index.html',        // Incluye el archivo index.html en el directorio public
+  ],
   theme: {
     extend: {},
   },
   plugins: [
+    // Aquí puedes agregar plugins de Tailwind si los necesitas en el futuro
   ],
 }
 
