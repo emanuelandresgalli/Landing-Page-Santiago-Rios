17774d75ecccba9a213f2fc04946e94dcdec9b8a
EmanuelGalli <emanuel.galli@bootcamp-upgrade.com>
2024-08-22 11:37:32 +0200

    Modificacion

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,11 @@
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Incluye todos los archivos .js, .jsx, .ts, .tsx en el directorio src
-    './public/index.html',        // Incluye el archivo index.html en el directorio public
-  ],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [
-    // Aquí puedes agregar plugins de Tailwind si los necesitas en el futuro
   ],
 }
 
