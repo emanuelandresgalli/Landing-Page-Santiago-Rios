7006fe0b201774a7d7e229ea3d3e4ca1d536887d
EmanuelGalli <emanuel.galli@bootcamp-upgrade.com>
2024-08-23 10:22:11 +0200

    Actualizo Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-import Navegacion from './src/Componentes/Navegacion';
+
 
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './src/Componentes/Navegacion.jsx',
-    'index.html',
    ],
   theme: {
     extend: {},
